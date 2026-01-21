73a3612edf19cd06d00dc06b0d57dab26ff31da6
Zodiac <di.zou1214@gmail.com>
2026-01-21 14:42:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# xFBO plugin for X-Plane
+
+## Credits
+hotbso for mentoring next gen XP developers https://github.com/hotbso 
+
---
+++ plugin.h
@@ -0,0 +1,42 @@
+//
+//    X Airline Snow: show accumulated snow in X-Plane's world
+//
+//    Copyright (C) 2025  Zodiac1214
+//    Copyright (C) 2025  Holger Teutsch
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+
+#ifndef _XA_SNOW_H_
+#define _XA_SNOW_H_
+
+#include <cstdint>
+#include <string>
+#include <tuple>
+#include <numbers>
+#include <memory>
+
+#include "XPLMDataAccess.h"
+#include "XPLMScenery.h"
+
+#include "log_msg.h"
+#include "http_get.h"
+
+extern std::string xp_dir;
+extern std::string plugin_dir;
+extern std::string output_dir;
+
+#endif
